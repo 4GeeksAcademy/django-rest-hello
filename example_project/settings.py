894a975a732309b9b281318e7fbcc75cd223dc59
Alejandro Sanchez <aalejo@gmail.com>
2019-04-27 09:52:46 +0000

    sample

--- example_project/settings.py
+++ example_project/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "@t33q!j@$(uj9-=qvpf=k46&es)mri!fa$sopf!g^!0v0zpx!5"
+SECRET_KEY = "replace_me"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
